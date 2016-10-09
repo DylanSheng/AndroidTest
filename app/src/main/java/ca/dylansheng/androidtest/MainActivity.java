bf95b35b8c7a05136f968af454eb292c633b8176
dylansheng <shengdiansd@gmail.com>
2016-10-09 14:31:39 -0600

    2

--- app/src/main/java/ca/dylansheng/androidtest/MainActivity.java
+++ app/src/main/java/ca/dylansheng/androidtest/MainActivity.java
@@ -2,6 +2,9 @@ package ca.dylansheng.androidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = (Button) findViewById(R.id.button_1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "clicked button 1", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
