a8a31c8c4aceec1b8f70296a42a3cbd4e13df1a6
dylansheng <shengdiansd@gmail.com>
2016-11-01 12:50:30 -0600

    add listview

--- app/src/main/java/ca/dylansheng/androidtest/MainActivity.java
+++ app/src/main/java/ca/dylansheng/androidtest/MainActivity.java
@@ -5,34 +5,25 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    private MyDatabaseHelper dbHelper;
+    private String[] data = {"Apple", "Banana", "Orange", "Watermelon",
+            "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Button button1 = (Button) findViewById(R.id.button_1);
 
-/*        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Second_Activity.class);
-                startActivity(intent);
-            }
-        });*/
-
-        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put("name", "The Da Vinci Code");
-        values.put("author", "Dan Brown");
-        values.put("pages", 454);
-        values.put("price", 16.96);
-        db.insert("book", null, values);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                MainActivity.this, android.R.layout.simple_list_item_1, data);
+        
+        ListView listView = (ListView) findViewById(R.id.listview_1);
+        listView.setAdapter(adapter);
     }
 }
