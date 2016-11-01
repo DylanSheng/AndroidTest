002052c873e6bac6353b732756f29f46783edb4f
dylansheng <shengdiansd@gmail.com>
2016-11-01 13:31:07 -0600

    add listview

--- app/src/main/java/ca/dylansheng/androidtest/MainActivity.java
+++ app/src/main/java/ca/dylansheng/androidtest/MainActivity.java
@@ -1,5 +1,6 @@
 package ca.dylansheng.androidtest;
 
+import android.app.Activity;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -11,19 +12,24 @@ import android.widget.ListView;
 import android.widget.Toast;
 import android.content.Intent;
 
-public class MainActivity extends AppCompatActivity {
-    private String[] data = {"Apple", "Banana", "Orange", "Watermelon",
-            "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango"};
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends Activity {
+    private List<Fruit> fruitList = new ArrayList<Fruit>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                MainActivity.this, android.R.layout.simple_list_item_1, data);
-        
+        initFruits(); // 初始化水果数据
+        FruitAdapter adapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item, fruitList);
         ListView listView = (ListView) findViewById(R.id.listview_1);
         listView.setAdapter(adapter);
+    }
+    private void initFruits() {
+        Fruit apple = new Fruit("Apple", R.mipmap.ic_launcher);
+        fruitList.add(apple);
+        Fruit banana = new Fruit("Banana", R.mipmap.ic_launcher);
+        fruitList.add(banana);
     }
 }
